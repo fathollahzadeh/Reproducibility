a11f63bd49ebf96dbfd8a8f6427ef87546aefb2f
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2024-01-19 18:35:48 -0500

    Clean-up, submit final readme and source code

--- sigmod2023-GIO-p454/run6PlotResults.sh
+++ sigmod2023-GIO-p454/run6PlotResults.sh
@@ -2,16 +2,16 @@
 
 cd explocal/plotting
 
-# rm -rf results # clean-up
-# mkdir -p results # create merge results path
+rm -rf results # clean-up
+mkdir -p results # create merge results path
 
 mkdir -p plots
-# python3 merge_results.py
+python3 merge_results.py
 
 make clean
 make
 make clean
 
-# rm -rf ../../plots # clean-up
-# mv exp2_identification_1k_10k_Table3.pdf plots/Table3.pdf
-# mv plots/ ../../
+rm -rf ../../plots # clean-up
+mv exp2_identification_1k_10k_Table3.pdf plots/Table3.pdf
+mv plots/ ../../
