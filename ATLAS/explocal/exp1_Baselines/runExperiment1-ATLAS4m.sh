d706a944a916311b371605ec53b02c335d0464ca
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2026-02-09 17:10:37 -0500

    update script

--- ATLAS/explocal/exp1_Baselines/runExperiment1-ATLAS4m.sh
+++ ATLAS/explocal/exp1_Baselines/runExperiment1-ATLAS4m.sh
@@ -12,10 +12,10 @@ source venv/bin/activate
 start=$(date +%s%N)
 python main.py ${matrix_data_path}
 end=$(date +%s%N)
-echo "ATLAS,JAX,"${itr}","$((($end - $start) / 1000000)) >>${root_path}/results/ABCD.dat
+echo "ABCD,ATLAS,"${itr}","$((($end - $start) / 1000000)) >>${root_path}/results/ABCD.dat
 
 
 start=$(date +%s%N)
 python main_Read.py ${matrix_data_path}
 end=$(date +%s%N)
-echo "ABCD,JAX,"${itr}","$((($end - $start) / 1000000)) >>${root_path}/results/ABCD_Read.dat
+echo "ABCD,ATLAS,"${itr}","$((($end - $start) / 1000000)) >>${root_path}/results/ABCD_Read.dat
