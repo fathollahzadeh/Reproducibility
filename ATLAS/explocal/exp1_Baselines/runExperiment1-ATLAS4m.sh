57d395bc7bf6467d15bfb5009185a713e5a09167
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2026-02-16 11:10:27 -0500

    update ATLAS

--- ATLAS/explocal/exp1_Baselines/runExperiment1-ATLAS4m.sh
+++ ATLAS/explocal/exp1_Baselines/runExperiment1-ATLAS4m.sh
@@ -2,7 +2,7 @@ itr=$1
 s=$2
 root_path="$(pwd)"
 data_path="${root_path}/data"
-matrix_data_path="${root_path}/data/matrix"
+matrix_data_path="${root_path}/data/matrix_${s}"
 path="${root_path}/setup"
 baseline_path="${path}/Baselines"
 
@@ -11,12 +11,12 @@ source venv/bin/activate
 
 
 start=$(date +%s%N)
-python main.py ${matrix_data_path}
+python task1.py ${matrix_data_path}
 end=$(date +%s%N)
-echo "ABCD,ATLAS,"${itr}","$((($end - $start) / 1000000)) >>${root_path}/results/ABCD.dat
+echo "TASK1,ATLAS,"${itr}","${s}","$((($end - $start) / 1000000)) >>${root_path}/results/TASK1.dat
 
 
 start=$(date +%s%N)
-python main_Read.py ${matrix_data_path}
+python task1_Read.py ${matrix_data_path}
 end=$(date +%s%N)
-echo "ABCD,ATLAS,"${itr}","$((($end - $start) / 1000000)) >>${root_path}/results/ABCD_Read.dat
+echo "TASK1,ATLAS,"${itr}","${s}","$((($end - $start) / 1000000)) >>${root_path}/results/TASK1_Read.dat
