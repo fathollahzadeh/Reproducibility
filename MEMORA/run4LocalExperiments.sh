20e3001a21675a509277ab6bbd38a79c56315077
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2026-02-22 09:54:12 -0500

    minor

--- MEMORA/run4LocalExperiments.sh
+++ MEMORA/run4LocalExperiments.sh
@@ -11,13 +11,13 @@ CMDExp1=./explocal/exp1_Baselines/runExperiment1-Calcite.sh
 clcite_exp1_log_fname="${root_path}/results/clcite_exp1.dat"
 echo "baseline,dataset_name,iteration,best_plan,enable_logical_rules,enable_physical_rules,time" > ${clcite_exp1_log_fname}
 
-for itr in {1..1}; do
-    # $CMDExp1 tpch PostgreSQL $itr false true false $clcite_exp1_log_fname true
-    # $CMDExp1 stackoverflow PostgreSQL $itr false true false $clcite_exp1_log_fname true
-    # $CMDExp1 imdb PostgreSQL $itr false true false $clcite_exp1_log_fname true
+for itr in {1..3}; do
+    $CMDExp1 tpch PostgreSQL $itr false true false $clcite_exp1_log_fname true
+    $CMDExp1 stackoverflow PostgreSQL $itr false true false $clcite_exp1_log_fname true
+    $CMDExp1 imdb PostgreSQL $itr false true false $clcite_exp1_log_fname true
 
     $CMDExp1 tpch PostgreSQL $itr true true true $clcite_exp1_log_fname true
-    # $CMDExp1 stackoverflow PostgreSQL $itr true true true $clcite_exp1_log_fname true
-    # $CMDExp1 imdb PostgreSQL $itr true true true $clcite_exp1_log_fname true
+    $CMDExp1 stackoverflow PostgreSQL $itr true true true $clcite_exp1_log_fname true
+    $CMDExp1 imdb PostgreSQL $itr true true true $clcite_exp1_log_fname true
            
 done   
