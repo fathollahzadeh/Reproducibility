1496b4ddf966a7afc4ca2e79968b4a18767d642a
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2026-02-26 11:53:42 -0500

    update results

--- MEMORA/run4LocalExperiments.sh
+++ MEMORA/run4LocalExperiments.sh
@@ -13,23 +13,24 @@ echo "baseline,dataset_name,iteration,best_plan,enable_logical_rules,enable_phys
 
 for itr in {1..1}; do
     $CMDExp1 tpch PostgreSQL $itr false true false $clcite_exp1_log_fname true
-    $CMDExp1 stackoverflow PostgreSQL $itr false true false $clcite_exp1_log_fname true
-    $CMDExp1 stackoverflow-1190 PostgreSQL $itr false true false $clcite_exp1_log_fname true
-    $CMDExp1 imdb PostgreSQL $itr false true false $clcite_exp1_log_fname true
-    $CMDExp1 dsb PostgreSQL $itr false true false $clcite_exp1_log_fname true
-    $CMDExp1 stats PostgreSQL $itr false true false $clcite_exp1_log_fname true
-    $CMDExp1 publicbibenchmark PostgreSQL $itr false true false $clcite_exp1_log_fname true
-    $CMDExp1 imdb_13k PostgreSQL $itr false true false $clcite_exp1_log_fname true
-    $CMDExp1 imdb_d PostgreSQL $itr false true false $clcite_exp1_log_fname true
+    # $CMDExp1 stackoverflow PostgreSQL $itr false true false $clcite_exp1_log_fname true
+    # $CMDExp1 stackoverflow-1190 PostgreSQL $itr false true false $clcite_exp1_log_fname true
+    # $CMDExp1 imdb PostgreSQL $itr false true false $clcite_exp1_log_fname true
+    # $CMDExp1 dsb PostgreSQL $itr false true false $clcite_exp1_log_fname true
+    # $CMDExp1 stats PostgreSQL $itr false true false $clcite_exp1_log_fname true
+    # $CMDExp1 publicbibenchmark PostgreSQL $itr false true false $clcite_exp1_log_fname true
+    # $CMDExp1 imdb_13k PostgreSQL $itr false true false $clcite_exp1_log_fname true
+    # $CMDExp1 imdb_d PostgreSQL $itr false true false $clcite_exp1_log_fname true
 
-    $CMDExp1 tpch PostgreSQL $itr true true true $clcite_exp1_log_fname true
-    $CMDExp1 stackoverflow PostgreSQL $itr true true true $clcite_exp1_log_fname true
-    $CMDExp1 stackoverflow-1190 PostgreSQL $itr true true true $clcite_exp1_log_fname true
-    $CMDExp1 imdb PostgreSQL $itr true true true $clcite_exp1_log_fname true
-    $CMDExp1 dsb PostgreSQL $itr true true true $clcite_exp1_log_fname true
-    $CMDExp1 stats PostgreSQL $itr true true true $clcite_exp1_log_fname true
-    $CMDExp1 publicbibenchmark PostgreSQL $itr true true true $clcite_exp1_log_fname true
-    $CMDExp1 imdb_13k PostgreSQL $itr true true true $clcite_exp1_log_fname true
-    $CMDExp1 imdb_d PostgreSQL $itr true true true $clcite_exp1_log_fname true
+ 
+    # $CMDExp1 tpch PostgreSQL $itr true true true $clcite_exp1_log_fname true
+    # $CMDExp1 stackoverflow PostgreSQL $itr true true true $clcite_exp1_log_fname true
+    # $CMDExp1 stackoverflow-1190 PostgreSQL $itr true true true $clcite_exp1_log_fname true
+    # $CMDExp1 imdb PostgreSQL $itr true true true $clcite_exp1_log_fname true
+    # $CMDExp1 dsb PostgreSQL $itr true true true $clcite_exp1_log_fname true
+    # $CMDExp1 stats PostgreSQL $itr true true true $clcite_exp1_log_fname true
+    # $CMDExp1 publicbibenchmark PostgreSQL $itr true true true $clcite_exp1_log_fname true
+    # $CMDExp1 imdb_13k PostgreSQL $itr true true true $clcite_exp1_log_fname true
+    # $CMDExp1 imdb_d PostgreSQL $itr true true true $clcite_exp1_log_fname true
                
 done   
